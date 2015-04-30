a5e67c47fcd7f44fa9aeae31e367dcef65d70c87
Moritz Lawitschka <me@moritzlawitschka.de>
2015-04-30 11:47:02 +0200

    Fix: Empty puppet config is still valid YAML

--- lib/validators/yaml_validator.rb
+++ lib/validators/yaml_validator.rb
@@ -1,6 +1,8 @@
 class YamlValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
+    return unless value.present?
+
     YAML.load(value).with_indifferent_access
   rescue StandardError
     record.errors.add(attribute, options[:message] || :not_yaml)

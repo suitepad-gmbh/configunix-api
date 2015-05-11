f56a27c4b0f7fe4ca3b5397696c7d473f8a4104f
Moritz Lawitschka <me@moritzlawitschka.de>
2015-05-11 15:10:13 +0200

    Only validate present  IP addresses and DNS names

--- app/models/host.rb
+++ app/models/host.rb
@@ -2,10 +2,10 @@ class Host < ActiveRecord::Base
 
   # Validations
   validates :instance_id, presence: true, uniqueness: true
-  validates :dns_name, hostname: { require_fqdn: true }
+  validates :dns_name, hostname: { require_fqdn: true }, if: -> { dns_name.present? }
   validates :private_dns_name, presence: true, hostname: { require_fqdn: true }
   validates :private_ip_address, presence: true, ip_address: true
-  validates :public_ip_address, ip_address: true
+  validates :public_ip_address, ip_address: true, if: -> { public_ip_address.present? }
   validates :puppet_config, yaml: true
 
 end

613f412a4c0c534d1828551e863ffcf5ae9f99ca
Moritz Lawitschka <me@moritzlawitschka.de>
2015-05-11 15:07:21 +0200

    Fix: Do not require public IP and DNS name

--- app/models/host.rb
+++ app/models/host.rb
@@ -2,10 +2,10 @@ class Host < ActiveRecord::Base
 
   # Validations
   validates :instance_id, presence: true, uniqueness: true
-  validates :dns_name, presence: true, hostname: { require_fqdn: true }
+  validates :dns_name, hostname: { require_fqdn: true }
   validates :private_dns_name, presence: true, hostname: { require_fqdn: true }
   validates :private_ip_address, presence: true, ip_address: true
-  validates :public_ip_address, presence: true, ip_address: true
+  validates :public_ip_address, ip_address: true
   validates :puppet_config, yaml: true
 
 end
